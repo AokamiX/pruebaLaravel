8b83eab728adb9471d26f7cb391a422eb07e1e50
AokamiX <alejandrojesus.cerpa@gmail.com>
2024-05-27 09:58:38 -0400

    numero 2

---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pedido;
+use Illuminate\Http\Request;
+use App\Models\PedidoProducto;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\PedidoCollection;
+
+class PedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new PedidoCollection(Pedido::with('user')->with('productos')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Almacenar una orden
+        $pedido = new Pedido;
+        $pedido->user_id = Auth::user()->id;
+        $pedido->total = $request->total;
+        $pedido->save();
+
+        //Obtener el ID del pedido
+        $id = $pedido->id;
+        
+        //Obtener los productos
+        $productos = $request->productos;
+
+
+        //Formatear un arreglo
+        $pedido_producto = [];
+
+        foreach($productos as $producto){
+            $pedido_producto[] = [
+                'pedido_id' => $id,
+                'producto_id' => $producto['id'],
+                'cantidad' => $producto['cantidad'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ];
+        }
+
+        //Almacenar en la DB
+        PedidoProducto::insert($pedido_producto);
+
+
+        return [
+            'message' => 'Pedido realizado correctamente, estará listo en unos minutos...'
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pedido $pedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pedido $pedido)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pedido  $pedido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pedido $pedido)
+    {
+        //
+    }
+}
